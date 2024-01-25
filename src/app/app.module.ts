d14e0ebf76a5a470d53076806146574aac0ed4b4
Matheus Bassi <bassi.matheus@outlook.com>
2024-01-25 00:19:37 -0300

    feat: Correcoes do grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderPrincipalComponent } from './components/header-principal/header-principal.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { CreateNoteComponent } from './components/notes/create-note/create-note.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderPrincipalComponent,
     FooterComponent,
-    BannerComponent
+    BannerComponent,
+    CreateNoteComponent
   ],
   imports: [
     BrowserModule,
