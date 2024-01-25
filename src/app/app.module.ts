e7ee3dee16b123b11516d809419bad5ea2a69192
Matheus Bassi <bassi.matheus@outlook.com>
2024-01-24 22:00:23 -0300

    feat: Implementando cabecalho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderPrincipalComponent } from './components/header-principal/header-principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderPrincipalComponent
   ],
   imports: [
     BrowserModule,
