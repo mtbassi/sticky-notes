b5f28dd7feded62bf063a01a8809586d50f7d6aa
Matheus Bassi <bassi.matheus@outlook.com>
2024-01-25 20:33:20 -0300

    feat: Implementando menu hamburguer

--- src/app/components/header-principal/header-principal.component.ts
+++ src/app/components/header-principal/header-principal.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header-principal',
   templateUrl: './header-principal.component.html',
-  styleUrls: ['./header-principal.component.css']
+  styleUrls: ['./header-principal.component.css'],
 })
 export class HeaderPrincipalComponent {
-
+  menuVariable: boolean = false;
+  menuIconVariable: boolean = false;
+  openMenu() {
+    this.menuVariable = !this.menuVariable;
+    this.menuIconVariable = !this.menuIconVariable;
+  }
 }
