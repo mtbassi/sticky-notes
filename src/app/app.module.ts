6f35a03860c7165df373a3c28858e7982868eb6b
Matheus Bassi <bassi.matheus@outlook.com>
2024-01-24 22:18:26 -0300

    feat: Implementando rodape

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderPrincipalComponent } from './components/header-principal/header-principal.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderPrincipalComponent
+    HeaderPrincipalComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
