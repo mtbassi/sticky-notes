20db29dd2c6f79fef14eaa817132cd672a3469cb
Matheus Bassi <bassi.matheus@outlook.com>
2024-01-24 23:42:11 -0300

    feat: Implementando Typed

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  styleUrls: ['./banner.component.css'],
 })
 export class BannerComponent {
+  ngOnInit() {
+    const options = {
+      strings: ['Turn your inspirations into a personal symphony of memories and reflections, where each note and word are eternal treasures'],
+      typeSpeed: 70,
+      backSpeed: 70,
+      showCursor: false,
+      loop: false
+ };
 
+ const typed = new Typed('.typed-element', options);
+  }
 }
